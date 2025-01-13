9c1722e6812969518eb7a784c76752676b12df74
Reuven Lerner <reuven@lerner.co.il>
2025-01-13 20:53:01 +0200

    gitautosync update 53

--- ubbi.py
+++ ubbi.py
@@ -2,6 +2,8 @@ def ubbi_word(word):
     output = []
 
     for one_character in word:
+        if not one_character.isalpha():
+            raise ValueError(f'{one_character} is illegal; stopping now')
         if one_character in 'aeiou':
             output.append('ub')
         output.append(one_character)
